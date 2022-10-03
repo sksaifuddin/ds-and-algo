b34b95633b8d8f70fc4dd1c142fa51f169a2984b
Saif <shaiksaifuddin95@gmail.com>
2022-10-03 17:42:49 +0530

    completed hill pattern

---
+++ patterns/Pattern5.java
@@ -0,0 +1,32 @@
+/*
+ 
+          *
+        * * *
+      * * * * *
+    * * * * * * *
+  * * * * * * * * * *
+ 
+*/
+
+
+class Pattern5 {
+  public static void main(String args[]) {
+    int n=5;
+    for(int i=1; i<=n; i++) {
+
+      for(int j=i; j<=n; j++){
+        System.out.print("  ");
+      }
+
+      for(int j=1; j<i; j++){
+        System.out.print("* ");
+      }
+
+      for(int j=1; j<=i; j++){
+        System.out.print("* "); 
+      }
+
+      System.out.println();
+    }
+  }
+}
